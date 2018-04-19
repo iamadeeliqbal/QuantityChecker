2bee2793d4cfc04cb5bdfd00b77c4d468e1e3bc0
adeel <its.adeeliqbal@gmail.com>
2018-04-19 12:37:49 +0500

    Check Quantity

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox_ac = New System.Windows.Forms.TextBox()
+        Me.label1 = New System.Windows.Forms.Label()
+        Me.label2 = New System.Windows.Forms.Label()
+        Me.TextBox_pn = New System.Windows.Forms.TextBox()
+        Me.Button_qty = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox_ac
+        '
+        Me.TextBox_ac.Location = New System.Drawing.Point(167, 102)
+        Me.TextBox_ac.Name = "TextBox_ac"
+        Me.TextBox_ac.Size = New System.Drawing.Size(227, 20)
+        Me.TextBox_ac.TabIndex = 0
+        '
+        'label1
+        '
+        Me.label1.AutoSize = True
+        Me.label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label1.Location = New System.Drawing.Point(23, 102)
+        Me.label1.Name = "label1"
+        Me.label1.Size = New System.Drawing.Size(138, 16)
+        Me.label1.TabIndex = 1
+        Me.label1.Text = "Enter Accesscode:"
+        '
+        'label2
+        '
+        Me.label2.AutoSize = True
+        Me.label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.label2.Location = New System.Drawing.Point(23, 161)
+        Me.label2.Name = "label2"
+        Me.label2.Size = New System.Drawing.Size(138, 16)
+        Me.label2.TabIndex = 2
+        Me.label2.Text = "Enter Part Number:"
+        '
+        'TextBox_pn
+        '
+        Me.TextBox_pn.Location = New System.Drawing.Point(167, 160)
+        Me.TextBox_pn.Name = "TextBox_pn"
+        Me.TextBox_pn.Size = New System.Drawing.Size(227, 20)
+        Me.TextBox_pn.TabIndex = 3
+        '
+        'Button_qty
+        '
+        Me.Button_qty.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button_qty.Location = New System.Drawing.Point(167, 222)
+        Me.Button_qty.Name = "Button_qty"
+        Me.Button_qty.Size = New System.Drawing.Size(102, 42)
+        Me.Button_qty.TabIndex = 4
+        Me.Button_qty.Text = "Get Quantity"
+        Me.Button_qty.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.Location = New System.Drawing.Point(292, 222)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(102, 42)
+        Me.Button2.TabIndex = 5
+        Me.Button2.Text = "Exit"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(182, 19)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(212, 29)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Quantity Checker"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(161, 58)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(479, 303)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button_qty)
+        Me.Controls.Add(Me.TextBox_pn)
+        Me.Controls.Add(Me.label2)
+        Me.Controls.Add(Me.label1)
+        Me.Controls.Add(Me.TextBox_ac)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Quantity Checker Form"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox_ac As TextBox
+    Friend WithEvents label1 As Label
+    Friend WithEvents label2 As Label
+    Friend WithEvents TextBox_pn As TextBox
+    Friend WithEvents Button_qty As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label3 As Label
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,116 @@
+﻿Imports System.IO
+Imports System.Net
+Imports System.Xml
+Public Class Form1
+
+    Dim acces_code As String
+    Dim part_number As String
+    Dim token As String
+    Dim webaddress As String
+    Dim xmldoc As New XmlDocument()
+    Dim xmldoc_invalid As New XmlDocument()
+    Dim xmldoc_part_num As New XmlDocument()
+    Dim xmldoc_data As New XmlDocument()
+    Dim xmldoc_data_pn As New XmlDocument()
+    Dim xmlnode_tkn As XmlNodeList
+    Dim xmlnode_invalid_tkn As XmlNodeList
+    Dim xmlnode_qty As XmlNodeList
+    Dim tkn As String
+    Dim tkn_invalid As String
+    Dim req_qty As String
+    Dim triger As Integer
+    Dim invalid_pn_list As XmlNodeList
+    Dim invalid_pn As String
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles label1.Click
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub TextBox_access_code_TextChanged(sender As Object, e As EventArgs) Handles TextBox_ac.TextChanged
+        acces_code = TextBox_ac.Text
+    End Sub
+
+    Private Sub TextBox_pn_TextChanged(sender As Object, e As EventArgs) Handles TextBox_pn.TextChanged
+        part_number = TextBox_pn.Text
+    End Sub
+
+    Private Sub Button_qty_Click(sender As Object, e As EventArgs) Handles Button_qty.Click
+        If acces_code = "" And part_number = "" Then
+            label1.ForeColor = Color.Red
+            label2.ForeColor = Color.Red
+            MessageBox.Show("Please Update Highlighted Values!", "Alert")
+            'triger = 0
+        End If
+        If acces_code = "" And part_number <> "" Then
+            label1.ForeColor = Color.Red
+            MessageBox.Show("Enter Access Code!", "Alert")
+        ElseIf acces_code <> "" Then
+            label1.ForeColor = Color.Black
+        End If
+        If part_number = "" And acces_code <> "" Then
+            label2.ForeColor = Color.Red
+            MessageBox.Show("Enter Part Number!", "Alert")
+        ElseIf part_number <> "" Then
+            label2.ForeColor = Color.Black
+        End If
+        If acces_code <> "" And part_number <> "" Then
+            triger = 1
+        End If
+        If triger = 1 Then
+            label1.ForeColor = Color.Black
+            label2.ForeColor = Color.Black
+            xmldoc_invalid.Load("http://juki-pc:8081/?f=login&accesscode=" & acces_code)
+            xmlnode_invalid_tkn = xmldoc_invalid.GetElementsByTagName("resdetail")
+            For y = 0 To xmlnode_invalid_tkn.Count - 1
+                tkn_invalid = xmlnode_invalid_tkn(y).InnerText
+            Next
+            If tkn_invalid = "invalid access code" Then
+                label1.ForeColor = Color.Red
+                MessageBox.Show("Enter Valid Access Code!!!", "Alert")
+            End If
+            xmldoc.Load("http://juki-pc:8081/?f=login&accesscode=" & acces_code)
+            xmlnode_tkn = xmldoc.GetElementsByTagName("token")
+            For x = 0 To xmlnode_tkn.Count - 1
+                tkn = xmlnode_tkn(x).InnerText
+            Next
+            xmldoc_data_pn.Load("http://juki-pc:8081/?f=item_get&item_code=" & part_number & "&tkn=" & tkn)
+            'webaddress = "http://juki-pc:8081/?f=item_get&item_code=" & part_number & "&tkn=" & tkn
+            'Process.Start(webaddress)
+            invalid_pn_list = xmldoc_data_pn.GetElementsByTagName("resdetail")
+            For z = 0 To invalid_pn_list.Count - 1
+                invalid_pn = invalid_pn_list(z).InnerText
+                If invalid_pn = "item not found" Then
+                    label2.ForeColor = Color.Red
+                    MessageBox.Show("Part number is incorrect or not exists!!", "Error")
+                End If
+            Next
+
+            'webaddress = "http://juki-pc:8081/?f=item_get&item_code=" & part_number & "&tkn=" & tkn
+            'Process.Start(webaddress)
+            Dim requ As WebRequest = WebRequest.Create("http://juki-pc:8081/?f=item_get&item_code=" & part_number & "&tkn=" & tkn)
+            requ.Timeout = 10 * 60 * 1000   ' 10 minutes timeout and not 100s as the default.
+            Dim response As WebResponse = requ.GetResponse()
+            Console.WriteLine("Will download {0:N0}bytes", response.ContentLength)
+            Dim stream As Stream = response.GetResponseStream()
+            xmldoc_data.Load(stream)
+            xmlnode_qty = xmldoc_data.GetElementsByTagName("stockquantity")
+            For i = 0 To xmlnode_qty.Count - 1
+                req_qty = xmlnode_qty(i).InnerText
+                MessageBox.Show(req_qty & " items exist in the database.", part_number)
+            Next
+        End If
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.Close()
+    End Sub
+
+    Private Sub Label3_Click(sender As Object, e As EventArgs) Handles Label3.Click
+
+    End Sub
+End Class
